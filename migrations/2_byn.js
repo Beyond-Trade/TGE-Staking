3d2a0dfc5c1739946fa70bf3322da09bf254a197
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-04-01 18:50:17 +0530

    feat: fix migratio s

--- migrations/2_byn.js
+++ migrations/2_byn.js
@@ -1,16 +1,28 @@
 const Mock = artifacts.require('Mock')
 const Mock2 = artifacts.require('Mock2')
 const BYN = artifacts.require('StakingFactory')
+const BYNLP = artifacts.require('StakingFactoryLP')
 
 module.exports = function (deployer) {
 	deployer.deploy(Mock, 'Reward', 'RWD').then((reward) => {
 		return deployer.deploy(Mock2, 'Staking', 'STK').then(async (staking) => {
 			return deployer.deploy(BYN, reward.address, Math.floor(Date.now() / 1000)).then(async (sf) => {
-				await reward.approve(sf.address, 100000000)
-				await reward.increaseAllowance(sf.address, 100000000)
+				await reward.approve(sf.address, 50000000)
+				await reward.increaseAllowance(sf.address, 50000000)
 
-				return sf.deploy(staking.address, 10000000).then(async () => {
-					return
+				return sf.deploy(staking.address, 50000000).then(async () => {
+					//
+					return deployer.deploy(Mock2, 'StakingLP', 'STL').then(async (staking) => {
+						//
+						return deployer.deploy(BYNLP, reward.address, Math.floor(Date.now() / 1000)).then(async (sf) => {
+							await reward.approve(sf.address, 50000000)
+							await reward.increaseAllowance(sf.address, 50000000)
+
+							return sf.deploy(staking.address, 5000000).then(async () => {
+								return
+							})
+						})
+					})
 				})
 			})
 		})
