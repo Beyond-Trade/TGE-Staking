4f0f2bfbe3f59a2854062afb57ad2d4ea2c6e9f2
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-04-01 21:20:30 +0530

    feat: update level check

--- contracts/LpStakingContract.sol
+++ contracts/LpStakingContract.sol
@@ -48,9 +48,20 @@ contract StakingLP is Ownable {
 		uint256 allowedReward;
 		uint256 alloted;
 	}
+
 	mapping(address => UserData) users;
 
-	function checkUpdateLevel(uint256 amount, uint256 level) public returns (uint256, uint256) {
+	constructor(
+		address _factory,
+		address _rewardsToken,
+		address _stakingToken
+	) {
+		factory = StakingFactoryLP(_factory);
+		rewardsToken = Mock(_rewardsToken);
+		stakingToken = IERC20(_stakingToken);
+	}
+
+	function checkUpdateLevel(uint256 amount, uint256 level) internal view returns (uint256, uint256) {
 		uint256 remaining;
 		(uint256 allowedForXCoins, uint256 _rewardPercentTimes100, uint256 _lockedDuration, uint256 _allowedReward, uint256 alloted) =
 			factory.levels(level);
@@ -63,22 +74,21 @@ contract StakingLP is Ownable {
 	}
 
 	/**deposit function */
-	function deposit(uint256 amount) public {
+	function deposit(uint256 amount) external {
 		// Check if level has been updated due to time elapsed.
-		uint256 level = factory.level();
-		if (level == 3) {
-			return;
-		}
+
 		factory.updateLevelCheck();
 		// Fetch the level.
 		uint256 remaining;
+		uint256 level = factory.level();
 		// Check if the amount updates the level and only deposit the amount which just updates
 		// and update the level. Return rest of the staking token to the user.
 		if (level == 1) {
 			(amount, remaining) = checkUpdateLevel(amount, level);
 			users[msg.sender].level1Tokens += amount;
 			stakingToken.transferFrom(msg.sender, address(this), amount);
-			factory.updateLevel(amount);
+			factory.updateTokens(amount);
+			
 			level = factory.level();
 			amount = remaining;
 		}
@@ -86,37 +96,68 @@ contract StakingLP is Ownable {
 			(amount, remaining) = checkUpdateLevel(amount, level);
 			users[msg.sender].level2Tokens += amount;
 			stakingToken.transferFrom(msg.sender, address(this), amount);
-			factory.updateLevel(amount);
+			factory.updateTokens(amount);
+			
 			level = factory.level();
 			amount = remaining;
 		}
 		if (level == 3) {
-			return;
+			(amount, remaining) = checkUpdateLevel(amount, level);
+			users[msg.sender].level3Tokens += amount;
+			stakingToken.transferFrom(msg.sender, address(this), amount);
+			factory.updateTokens(amount);
+			
+			level = factory.level();
+			amount = remaining;
 		}
+		if (level == 4) {
+			(amount, remaining) = checkUpdateLevel(amount, level);
+			users[msg.sender].level4Tokens += amount;
+			stakingToken.transferFrom(msg.sender, address(this), amount);
+			factory.updateTokens(amount);
+			level = factory.level();
+			amount = remaining;
+		}
+
 		users[msg.sender].lastUpdateDate = block.timestamp;
-		factory.updateTokens(amount);
 	}
 
-	function calculateReward() public returns (UserData memory user) {
-		if (users[msg.sender].level1Tokens != 0) {
+	function calculateReward() public view returns (UserData memory user) {
+		UserData memory userDetails = users[msg.sender];
+		//
+		if (userDetails.level1Tokens != 0) {
 			(uint256 allowedForXCoins, uint256 _rewardPercentTimes100, uint256 _lockedDuration, uint256 _allowedReward, uint256 alloted) =
 				factory.levels(1);
 			if (block.timestamp > factory.startTime() + _lockedDuration * oneDay) {
-				users[msg.sender].level1Reward = ((_rewardPercentTimes100 * users[msg.sender].level1Tokens)) / 10000;
+				userDetails.level1Reward = ((_rewardPercentTimes100 * userDetails.level1Tokens)) / 10000;
 			}
 		}
-		if (users[msg.sender].level2Tokens != 0) {
+		if (userDetails.level2Tokens != 0) {
 			(uint256 allowedForXCoins, uint256 _rewardPercentTimes100, uint256 _lockedDuration, uint256 _allowedReward, uint256 alloted) =
 				factory.levels(2);
 			if (block.timestamp > factory.startTime() + _lockedDuration * oneDay) {
-				users[msg.sender].level2Reward = ((_rewardPercentTimes100 * users[msg.sender].level2Tokens)) / 10000;
+				userDetails.level2Reward = ((_rewardPercentTimes100 * userDetails.level2Tokens)) / 10000;
 			}
 		}
-		return users[msg.sender];
+		if (userDetails.level3Tokens != 0) {
+			(uint256 allowedForXCoins, uint256 _rewardPercentTimes100, uint256 _lockedDuration, uint256 _allowedReward, uint256 alloted) =
+				factory.levels(3);
+			if (block.timestamp > factory.startTime() + _lockedDuration * oneDay) {
+				userDetails.level3Reward = ((_rewardPercentTimes100 * userDetails.level3Tokens)) / 10000;
+			}
+		}
+		if (userDetails.level4Tokens != 0) {
+			(uint256 allowedForXCoins, uint256 _rewardPercentTimes100, uint256 _lockedDuration, uint256 _allowedReward, uint256 alloted) =
+				factory.levels(4);
+			if (block.timestamp > users[msg.sender].lastUpdateDate + _lockedDuration * oneDay) {
+				userDetails.level4Reward = ((_rewardPercentTimes100 * userDetails.level4Tokens)) / 10000;
+			}
+		}
+		return userDetails;
 	}
 
 	// Withdraw.
-	function withdraw(uint256 level) public {
+	function withdraw(uint256 level) external {
 		if (level == 1 && users[msg.sender].level1Tokens != 0) {
 			(uint256 allowedForXCoins, uint256 _rewardPercentTimes100, uint256 _lockedDuration, uint256 _allowedReward, uint256 alloted) =
 				factory.levels(1);
@@ -140,18 +181,30 @@ contract StakingLP is Ownable {
 				users[msg.sender].level2Reward = 0;
 				return;
 			}
+		} else if (level == 3 && users[msg.sender].level3Tokens != 0) {
+			(uint256 allowedForXCoins, uint256 _rewardPercentTimes100, uint256 _lockedDuration, uint256 _allowedReward, uint256 alloted) =
+				factory.levels(3);
+			if (block.timestamp > factory.startTime() + _lockedDuration * oneDay) {
+				uint256 rewardValue = users[msg.sender].level3Tokens + (((_rewardPercentTimes100 * users[msg.sender].level3Tokens)) / 10000);
+				rewardsToken.approveInternal(address(this), msg.sender, rewardValue);
+				rewardsToken.transferInternal(address(this), msg.sender, rewardValue);
+				users[msg.sender].level3Tokens = 0;
+				users[msg.sender].level3Reward = 0;
+				return;
+			}
+		} else if (level == 4 && users[msg.sender].level4Tokens != 0) {
+			(uint256 allowedForXCoins, uint256 _rewardPercentTimes100, uint256 _lockedDuration, uint256 _allowedReward, uint256 alloted) =
+				factory.levels(4);
+			if (block.timestamp > factory.startTime() + _lockedDuration * oneDay) {
+				uint256 rewardValue = users[msg.sender].level4Tokens + (((_rewardPercentTimes100 * users[msg.sender].level4Tokens)) / 10000);
+				rewardsToken.approveInternal(address(this), msg.sender, rewardValue);
+				rewardsToken.transferInternal(address(this), msg.sender, rewardValue);
+				users[msg.sender].level4Tokens = 0;
+				users[msg.sender].level4Reward = 0;
+				return;
+			}
 		}
 		require(false, 'Cannot withdraw');
-	}
-
-	constructor(
-		address _factory,
-		address _rewardsToken,
-		address _stakingToken
-	) {
-		factory = StakingFactoryLP(_factory);
-		rewardsToken = Mock(_rewardsToken);
-		stakingToken = IERC20(_stakingToken);
 	}
 }
 
@@ -175,36 +228,11 @@ contract StakingFactoryLP is Ownable {
 		uint256 alloted;
 	}
 
+	address public stakingContractAddress;
 	// rewards info by staking token
 	mapping(address => StakingRewardsInfo) public stakingRewardsInfoByStakingToken;
 
 	mapping(uint256 => LevelData) public levels;
-
-	// Create Levels
-	// TODO: init in constructor?
-	// Or create an array and provide user access to create them.
-	function createLevels() internal onlyOwner {
-		levels[1] = LevelData(300000, 9900, 30, 295890, 0);
-		levels[2] = LevelData(200000, 4100, 30, 82192, 0);
-
-		level = 1;
-	}
-
-	function updateLevel(uint256 tokenValue) public {
-		if (levels[level].alloted + tokenValue >= levels[level].allowedForXCoins) {
-			level += 1;
-		}
-	}
-
-	function updateTokens(uint256 tokenValue) public {
-		levels[level].alloted += tokenValue;
-	}
-
-	function updateLevelCheck() public {
-		if (block.timestamp > startTime + 60 * 24 * 60) {
-			level = 4;
-		}
-	}
 
 	constructor(address _rewardsToken, uint256 _startTime) {
 		rewardsToken = Mock(_rewardsToken);
@@ -217,11 +245,46 @@ contract StakingFactoryLP is Ownable {
 	function deploy(address stakingToken, uint256 rewardAmount) public onlyOwner {
 		StakingRewardsInfo storage info = stakingRewardsInfoByStakingToken[stakingToken];
 		require(info.stakingRewards == address(0), 'StakingRewardsFactory::deploy: already deployed');
+
 		info.stakingRewards = address(new StakingLP(address(this), address(rewardsToken), stakingToken));
+
+		stakingContractAddress = info.stakingRewards;
 
 		rewardsToken.approveInternal(msg.sender, info.stakingRewards, rewardAmount);
 		rewardsToken.transferInternal(msg.sender, info.stakingRewards, rewardAmount);
 		info.rewardAmount = rewardAmount;
 		stakingTokens.push(stakingToken);
+	}
+
+	// Create Levels
+	// Or create an array and provide user access to create them.
+	function createLevels() internal {
+		levels[1] = LevelData(100000, 4900, 15, 49315, 0);
+		levels[2] = LevelData(400000, 4100, 30, 123288, 0);
+		levels[3] = LevelData(800000, 4300, 45, 172603, 0);
+		levels[4] = LevelData(1500000, 3300, 60, 230137, 0);
+
+		level = 1;
+	}
+
+	modifier restricted() {
+		require(msg.sender == stakingContractAddress, 'Sender has to be a staking contract.');
+		_;
+	}
+
+	function updateTokens(uint256 tokenValue) external restricted {
+		levels[level].alloted += tokenValue;
+		if(level==4){
+			return;
+		}
+		if (levels[level].alloted >= levels[level].allowedForXCoins) {
+			level += 1;
+		}
+	}
+
+	function updateLevelCheck() external {
+		if (block.timestamp > startTime + 60 * 24 * 60) {
+			level = 4;
+		}
 	}
 }
