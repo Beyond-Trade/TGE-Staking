de6c3dfc1fe80a97a57c46d8705b7104359596b9
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-04-13 19:23:34 +0530

    docsfix: added instructions

--- migrations/2_byn.js
+++ migrations/2_byn.js
@@ -16,33 +16,26 @@ const data = {
 	mock_token_2: '',
 	staking_factory: '',
 	staking_factory_lp: '',
+	// Only byn_token needs to be updated. Rest will added by the script.
 	byn_token: '0x6A1f09B3Afe036D595D5487C911bAd7cE271582B',
 }
 
 module.exports = function (deployer) {
-	// deployer.deploy(Mock, 'Reward', 'RWD').then((reward) => {
-	// return deployer.deploy(Mock2, 'Staking', 'STK').then(async (staking) => {
+	// Use Beyond artifact here
 	const Beyond = new ERC20(data.byn_token)
+
 	return deployer.deploy(BYN, data.byn_token, Math.floor(DEPLOYMENT_TIMESTAMP / 1000)).then(async (sf) => {
 		data.reward_token = data['byn_token']
 		data.mock_token_1 = data['byn_token']
+		data.mock_token_2 = data['byn_token']
 
 		data.staking_factory = sf.address
+		data.staking_factory_lp = sf.address
 
-		// return deployer.deploy(Mock2, 'StakingLP', 'STL').then(async (staking) => {
-		// 	// 		//
-		// 	return deployer.deploy(BYNLP, reward.address, Math.floor(Date.now() / 1000)).then(async (sf) => {
 		await Beyond.approve(sf.address, new BigNumber(500000 * Math.pow(10, 18)))
+
 		await Beyond.increaseAllowance(sf.address, new BigNumber(500000 * Math.pow(10, 18)))
 		await sf.deploy(data.byn_token, new BigNumber(2000 * Math.pow(10, 18)))
-		// 		data.mock_token_2 = staking.address
-		// 		data.staking_factory_lp = sf.address
 		return writeFileSync(join(__dirname, '..', 'client', 'src', 'addresses.json'), JSON.stringify(data, undefined, 4))
-		// return sf.deploy(staking.address, new BigNumber(500000 * Math.pow(10, 18))).then(async () => {
-		// return
-		// })
-		// })
-		// })
 	})
-	// })
 }
